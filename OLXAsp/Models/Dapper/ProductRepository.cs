36b50f5a3bf4354ab04170c9d2e91e1fa042d718
student <student@COMP-507-1-12>
2021-09-26 13:15:08 +0300

    Add project files.

---
+++ OLXAsp/Models/Dapper/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OLXAsp.Models.Dapper
+{
+    public class Product
+    {
+        public int Product_Id { get; set; }
+        public string Product_ImageURL { get; set; }
+        public string Product_Description { get; set; }
+        public decimal Product_Price { get; set; }
+        public int Product_CategoryId { get; set; }
+    }
+    public class ProductRepository
+    {
+
+    }
+}
